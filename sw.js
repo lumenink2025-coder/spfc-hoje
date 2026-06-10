140c049096448eb3aa235f46ec7f8554a573e676
lumenink2025-coder <lumenink2025@gmail.com>
2026-06-09 21:36:29 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // SPFC — Hoje na História | Service Worker v1.1.1.1
 // ⚠️ MUDE O CACHE_VERSION A CADA DEPLOY!
 
-const CACHE_VERSION = "v1.1.1.1.1.1.0.0.1.7.4";  // ← ALTERE ISSO A CADA NOVO DEPLOY
+const CACHE_VERSION = "v1.1.1.1.1.1.0.0.1.7.5";  // ← ALTERE ISSO A CADA NOVO DEPLOY
 const CACHE_NAME = `spfc-hoje-${CACHE_VERSION}`;
 const ASSETS = ['/', '/index.html', '/quiz.html', '/manifest.json'];
 
